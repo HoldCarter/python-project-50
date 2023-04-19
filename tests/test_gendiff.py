7495c51348c135612faf25d6d08038100d896fcf
HoldCarter <holdcarter@holdcarter.ru>
2023-04-19 15:14:10 +0300

    modified_path_tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,8 +3,8 @@ from gendiff import generate_diff
 
 
 def test_generate_diff():
-    plain_test_path = './fixtures/plain_files_test.txt'
+    plain_test_path = './tests/fixtures/plain_files_test.txt'
     with open(plain_test_path, encoding='utf8') as f:
         plain_test = f.read().strip()
         
-    assert generate_diff("fixtures/file1.json", "fixtures/file2.json") == plain_test
+    assert generate_diff("./tests/fixtures/file1.json", "./tests/fixtures/file2.json") == plain_test
