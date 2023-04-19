3d5a1045064c144ad1c3ed674de78ef29db435ba
HoldCarter <holdcarter@holdcarter.ru>
2023-04-19 15:06:15 +0300

    correct test_path

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,9 +1,10 @@
 import pytest
-from gendiff import generate_diff, take_args
+from gendiff import generate_diff
 
 
 def test_generate_diff():
-    plain_test_path = 'fixtures/plain_files_test.txt'
+    plain_test_path = './fixtures/plain_files_test.txt'
     with open(plain_test_path, encoding='utf8') as f:
         plain_test = f.read().strip()
-        assert generate_diff("fixtures/file1.json", "fixtures/file2.json") == plain_test
+        
+    assert generate_diff("fixtures/file1.json", "fixtures/file2.json") == plain_test
